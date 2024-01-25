5130252bce3ef794b2e039f5633b042481e8dc0a
Musstah <pythonapilearn@gmail.com>
2024-01-25 13:50:53 +0100

    Revert "Reverting back because of ussues"
    This reverts commit 102b7158a1bcc49523420d8232587ea6b7f804f5.

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -2,32 +2,23 @@ import PropTypes from "prop-types";
 
 function FeedbackStats({ feedback }) {
   // Calculate ratings avg
-  if (!feedback) {
-    return (
-      <div className="feedback-stats">
-        <h4>No Reviews</h4>
-        <h4>Average Rating: 0</h4>
-      </div>
-    );
-  } else {
-    let average =
-      feedback.reduce((acc, cur) => {
-        return acc + cur.rating;
-      }, 0) / feedback.length;
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
 
-    return (
-      <div className="feedback-stats">
-        <h4>{feedback.length} Reviews</h4>
-        <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
-      </div>
-    );
-  }
+  average = average.toFixed(1).replace(/[.,]0$/, "");
 
-  // average = average.toFixed(1).replace(/[.,]0$/, "");
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
 }
 
-// FeedbackStats.propTypes = {
-//   feedback: PropTypes.array.isRequired,
-// };
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
 
 export default FeedbackStats;
