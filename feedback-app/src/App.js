102b7158a1bcc49523420d8232587ea6b7f804f5
Musstah <pythonapilearn@gmail.com>
2024-01-25 13:41:04 +0100

    Reverting back because of ussues

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import FeedbackData from "./data/FeedbackData";
 import Card from "./components/shared/Card";
-import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
 
@@ -37,7 +37,7 @@ function App() {
                 <FeedbackList />
               </>
             }
-          ></Route>
+          />
 
           <Route path="/about" element={<AboutPage />} />
         </Routes>
--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -2,23 +2,32 @@ import PropTypes from "prop-types";
 
 function FeedbackStats({ feedback }) {
   // Calculate ratings avg
-  let average =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating;
-    }, 0) / feedback.length;
+  if (!feedback) {
+    return (
+      <div className="feedback-stats">
+        <h4>No Reviews</h4>
+        <h4>Average Rating: 0</h4>
+      </div>
+    );
+  } else {
+    let average =
+      feedback.reduce((acc, cur) => {
+        return acc + cur.rating;
+      }, 0) / feedback.length;
 
-  average = average.toFixed(1).replace(/[.,]0$/, "");
+    return (
+      <div className="feedback-stats">
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+      </div>
+    );
+  }
 
-  return (
-    <div className="feedback-stats">
-      <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
-    </div>
-  );
+  // average = average.toFixed(1).replace(/[.,]0$/, "");
 }
 
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-};
+// FeedbackStats.propTypes = {
+//   feedback: PropTypes.array.isRequired,
+// };
 
 export default FeedbackStats;
