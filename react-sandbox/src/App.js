634fbfab8ce38fdd54cedddbba8529ddf8c9db87
Musstah <pythonapilearn@gmail.com>
2024-01-31 14:41:58 +0100

    Stopped at memo

---
+++ react-sandbox/src/App.js
@@ -0,0 +1,11 @@
+import UseMemoExample from "./components/UseMemoExample";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <UseMemoExample />
+    </div>
+  );
+}
+
+export default App;
