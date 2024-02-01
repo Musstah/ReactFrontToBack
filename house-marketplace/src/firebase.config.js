b5106a142f865287394b4afe1ae5d704b0762d84
Musstah <pythonapilearn@gmail.com>
2024-02-01 10:26:40 +0100

    Marketplace project started

---
+++ house-marketplace/src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6ZZypuDsZFBVsSZCAekXZ83k_A9mnlX8",
+  authDomain: "house-marketplace-app-87cff.firebaseapp.com",
+  projectId: "house-marketplace-app-87cff",
+  storageBucket: "house-marketplace-app-87cff.appspot.com",
+  messagingSenderId: "233608167557",
+  appId: "1:233608167557:web:f8315d33178f8c149569ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
